fe7623e0dd32b5f9a30ee019f33c27b9bc1cf934
Jorge Guillen <jguillenm1500@alumno.ipn.mx>
2022-05-08 18:17:57 -0500

    Add test for filterByMission method

---
+++ test/services/ExplorerService.test.js
@@ -0,0 +1,29 @@
+const Reader = require('./../../lib/utils/Reader');
+const ExplorerService = require('./../../lib/services/ExplorerService');
+
+const jsonFile = Reader.readJsonFile('test/data/explorersDemo.json');
+
+describe('Tests for ExplorerService class', () => {
+	test('Method to filter by mission', () => {
+		const explorersInNode = ExplorerService.filterByMission(
+			jsonFile,
+			'node'
+		);
+		expect(explorersInNode).toEqual([
+			{
+				name: 'Woopa1',
+				githubUsername: 'ajolonauta1',
+				score: 3,
+				mission: 'node',
+				stacks: ['javascript', 'reasonML', 'elm'],
+			},
+			{
+				name: 'Woopa3',
+				githubUsername: 'ajolonauta3',
+				score: 15,
+				mission: 'node',
+				stacks: ['elixir', 'groovy', 'reasonML'],
+			},
+		]);
+	});
+});
