2e91dd68f74010a40ccb98ad3fa0542276c4d245
Jorge Guillen <jguillenm1500@alumno.ipn.mx>
2022-05-09 17:54:46 -0500

    Add test for getExplorerUsernamesByMission method

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -5,4 +5,21 @@ describe("Tests for ExplorerController class", () => {
         const explorersInNode = ExplorerController.getExplorerByMission("node");
         expect(explorersInNode).toHaveLength(10);
     });
+
+    test("Get explorers usernames by mission", () => {
+        const explorersUsernames =
+            ExplorerController.getExplorersUsernamesByMission("node");
+        expect(explorersUsernames).toEqual([
+            "ajolonauta1",
+            "ajolonauta2",
+            "ajolonauta3",
+            "ajolonauta4",
+            "ajolonauta5",
+            "ajolonauta11",
+            "ajolonauta12",
+            "ajolonauta13",
+            "ajolonauta14",
+            "ajolonauta15",
+        ]);
+    });
 });
