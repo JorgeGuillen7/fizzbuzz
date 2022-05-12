3f0bc7c34efb28d4135714a2169457f4fa6533c3
Jorge Guillen <jguillenm1500@alumno.ipn.mx>
2022-05-12 00:54:55 -0500

    Add appyValidationInNumber method in FizzbuzzService.js

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -4,7 +4,7 @@ describe("Tests for FizzbuzzService class", () => {
     test("Validation if the score is divisible by 3 and 5", () => {
         const explorer = { name: "Explorer", score: 15 };
         const explorerValidation =
-			FizzbuzzService.applyValidationInExplorer(explorer);
+            FizzbuzzService.applyValidationInExplorer(explorer);
         expect(explorerValidation).toEqual({
             name: "Explorer",
             score: 15,
@@ -15,7 +15,7 @@ describe("Tests for FizzbuzzService class", () => {
     test("Validation if the score is divisible by 5", () => {
         const explorer = { name: "Explorer", score: 5 };
         const explorerValidation =
-			FizzbuzzService.applyValidationInExplorer(explorer);
+            FizzbuzzService.applyValidationInExplorer(explorer);
         expect(explorerValidation).toEqual({
             name: "Explorer",
             score: 5,
@@ -26,7 +26,7 @@ describe("Tests for FizzbuzzService class", () => {
     test("Validation if the score is divisible by 3", () => {
         const explorer = { name: "Explorer", score: 3 };
         const explorerValidation =
-			FizzbuzzService.applyValidationInExplorer(explorer);
+            FizzbuzzService.applyValidationInExplorer(explorer);
         expect(explorerValidation).toEqual({
             name: "Explorer",
             score: 3,
@@ -37,11 +37,23 @@ describe("Tests for FizzbuzzService class", () => {
     test("Validation if the score is not divisible by 3 and 5", () => {
         const explorer = { name: "Explorer", score: 1 };
         const explorerValidation =
-			FizzbuzzService.applyValidationInExplorer(explorer);
+            FizzbuzzService.applyValidationInExplorer(explorer);
         expect(explorerValidation).toEqual({
             name: "Explorer",
             score: 1,
             trick: 1,
         });
+    });
+
+    test("Fizzbuzz validation receiving a number", () => {
+        const explorerScore1 = FizzbuzzService.applyValidationInNumber(1);
+        const explorerScore3 = FizzbuzzService.applyValidationInNumber(3);
+        const explorerScore5 = FizzbuzzService.applyValidationInNumber(5);
+        const explorerScore15 = FizzbuzzService.applyValidationInNumber(15);
+
+        expect(explorerScore1).toEqual({ score: 1, trick: 1 });
+        expect(explorerScore3).toEqual({ score: 3, trick: "Fizz" });
+        expect(explorerScore5).toEqual({ score: 5, trick: "Buzz" });
+        expect(explorerScore15).toEqual({ score: 15, trick: "Fizzbuzz" });
     });
 });
